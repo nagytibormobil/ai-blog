26e375647c97d9cb9274752b6c9c4f73eb2c1220
Tibor Nagy <nagytibormobil@gmail.com>
2025-09-11 07:19:15 +0200

    YouTube jav├şt├ís ├ęs HTML update !TODAY! !NOW!

--- youtubefix.py
+++ youtubefix.py
@@ -7,7 +7,7 @@ RAWG_API_KEY = "2fafa16ea4c147438f3b0cb031f8dbb7"
 
 # Mappa, ahol a HTML fájlok vannak
 folder = r"C:\ai_blog\generated_posts"
-# Default YouTube videó, ha hiányzik
+# Default YouTube videó, ha hiányzik vagy helytelen
 default_video = "https://www.youtube.com/embed/iGP8SqoEQOQ"
 
 # Végigmegyünk minden .html fájlon
@@ -17,16 +17,23 @@ for filename in os.listdir(folder):
         with open(filepath, "r", encoding="utf-8") as f:
             content = f.read()
 
-        # Ellenőrizzük, van-e <iframe> YouTube link
-        if "<iframe" not in content or "youtube.com/embed" not in content:
-            print(f"Hiányzó videó: {filename}")
-            # Beillesztjük a default videót a <h2>Gameplay Video</h2> alá
-            new_content = re.sub(
-                r"(<h2>Gameplay Video</h2>)",
-                r"\1\n<iframe width=\"100%\" height=\"400\" src=\"" + default_video + "\" frameborder=\"0\" allowfullscreen></iframe>",
-                content
-            )
+        # Ellenőrizzük, van-e <iframe> YouTube embed link
+        youtube_embed = re.search(r'<iframe[^>]+src=["\']https://www\.youtube\.com/embed/[^"\']+', content)
+        if not youtube_embed:
+            print(f"Hiányzó vagy nem megfelelő videó: {filename}")
+            # Ellenőrizzük, van-e <h2>Gameplay Video</h2>
+            if "<h2>Gameplay Video</h2>" in content:
+                # Beillesztjük a default videót a <h2>Gameplay Video</h2> alá
+                new_content = re.sub(
+                    r"(<h2>Gameplay Video</h2>)",
+                    r"\1\n<iframe width=\"100%\" height=\"400\" src=\"" + default_video + "\" frameborder=\"0\" allowfullscreen></iframe>",
+                    content
+                )
+            else:
+                # Ha nincs <h2>, akkor a fájl végére illesztjük
+                new_content = content + f"\n<h2>Gameplay Video</h2>\n<iframe width=\"100%\" height=\"400\" src=\"{default_video}\" frameborder=\"0\" allowfullscreen></iframe>"
+
             with open(filepath, "w", encoding="utf-8") as f:
                 f.write(new_content)
 
-print("YouTube ellenőrzés kész.")
+print("YouTube ellenőrzés és javítás kész.")
